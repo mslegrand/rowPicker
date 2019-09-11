ec27709594d182a4b1c991947713f13b08803b9b
mslegrand <slegrand3.14@gmail.com>
2019-09-11 15:31:12 -0400

    cosemetic

--- inst/www/rowPicker.js
+++ inst/www/rowPicker.js
@@ -1,37 +1,31 @@
 var rowPickerBinding = new Shiny.InputBinding();
 $.extend(rowPickerBinding, {
   find: function(scope) {
-    
     return $(scope).find(".rowPicker");
   },
   initialize: function(el){
     //  Initialize any data values here
-    // here we initial Z and the current Index
     var inputId1=el.id;
-    //console.log('inputId1='+el.id);
     var inputId2=el.id+'-list';
-    //console.log('inputId2='+inputId2);
     var downId=el.id+"-scrollDown";
-    //console.log('downId='+downId);
     var upId=el.id+"-scrollUp";
-    ////console.log('downId='+upId);
     var rowScroller = new SnippetToolBaR( inputId1, inputId2, downId, upId, 32);
-    
+    // add to element
     $(el).data("rowScroller", rowScroller);
-    
+
     $(el).data("rowScroller").reAdjustPos();
     $(rowScroller.downId).click(function(){$(el).data("rowScroller").onDownClick();});
     $(rowScroller.upId).click(function(){ $(el).data("rowScroller").onUpClick();});
-    
+
     $(window).on('resize',function(e){  $(el).data("rowScroller").reAdjustPos(); });
-    
+
 	  $("#"+inputId2).sortable(
-	    { 
+	    {
 	      update:function(event, ui){
 	      console.log('hello');
 	      $(el).trigger("change");
 	    }
-	    
+
 	  } );
 
     let rpl = new RowPickerList(inputId1, inputId2);
@@ -39,12 +33,7 @@ $.extend(rowPickerBinding, {
     let count = $(el).attr(`data-count`);
     count=JSON.parse(count);
     $(el).data("rowList").populateRows(count);
-    //this.(el, setValue
     $(el).data('rowScroller').reAdjustPos();
-    
-    //let iniRowList=$(el).attr(`data-rowList`); //extract attribute
-    //$(el).data("rowList", JSON.parse(iniRowList)); //convert to an object and attach
-    //$(el).data('Index',0); // set index
   },
   getValue: function(el) {
     let selected = $(el).data("rowList").getSelected();
@@ -56,7 +45,7 @@ $.extend(rowPickerBinding, {
       order: JSON.stringify(order)
     };
   },
-  setValue: function(el, value) { 
+  setValue: function(el, value) {
     // used for updating input control
     if(!!value.count){
       if(value.count>0){
@@ -87,18 +76,18 @@ $.extend(rowPickerBinding, {
     }
   },
   subscribe: function(el, callback) {
-    // notify server whenever change 
+    // notify server whenever change
       $(el).on("change.rowPickerBinding", function(e) {
       callback(false);
     });
   },
   unsubscribe: function(el) {
-    $(el).off(".rowPickerBinding");                              
+    $(el).off(".rowPickerBinding");
   },
-  receiveMessage: function(el, data) { //called by server when updating 
-   
+  receiveMessage: function(el, data) { //called by server when updating
+
     if(!!data.count){
-      let count = Number( data.count); 
+      let count = Number( data.count);
       console.log('data.count='+JSON.stringify(count));
       this.setValue(el, {count: count});
     }
@@ -117,10 +106,8 @@ $.extend(rowPickerBinding, {
     if(!!data.selectRow){
       this.setValue(el, {selectRow:data.selectRow});
     }
-    
   },
-  
-  getType: function(el){ 
+  getType: function(el){
     return "rowPickerBinding";
   }
 });
