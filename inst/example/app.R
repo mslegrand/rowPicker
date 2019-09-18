2df3ca1118c4ad9cb7cdedef145364167b7487a4
m. s. legrand <slegrand3.14@gmail.com>
2019-09-18 12:02:12 -0400

    more tests in app

--- inst/example/app.R
+++ inst/example/app.R
@@ -13,27 +13,52 @@ library(rowPicker)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     basicPage(
+
         rowPicker(inputId="myTibRowCntrl", count=5),
-        fixedPanel(top=0, left=100,
-           span( tags$b('selected:'),
-                textOutput(outputId = 'selection')
+        fixedPanel(top=10, left=100,
+
+           div(  tags$b('selected:'),
+                div(style="display:inline-block;vertical-align:top;",
+                    textOutput(outputId = 'selection'))
             ),
-           span( tags$b('group:'),
-                 textOutput(outputId = 'group')
+           div( tags$b('group:'),
+                 div(style="display:inline-block;vertical-align:top;",
+                 textOutput(outputId = 'group'))
            ),
-           span( tags$b('order:'),
-                 textOutput(outputId = 'order')
+           div( tags$b('order:'),
+                 div(style="display:inline-block;vertical-align:top;",
+                 textOutput(outputId = 'order'))
+                ,actionButton(inputId='renumber', label='renumber')
            ),
-           span( tags$b('count:'),
-                 textOutput(outputId = 'count')
-           ),
-           numericInput(inputId = 'size',
-                label='Reset Size',
-                10, min = 0, max = 100, step = NA,
-                width = 100
+           div( tags$b('count:'),
+                 div(style="display:inline-block;vertical-align:top;",
+                 textOutput(outputId = 'count'))
             ),
-           actionButton(inputId='set', label='set'),
-           actionButton(inputId='renumber', label='renumber')
+            div(style="display:inline-block;vertical-align:top;",
+                numericInput(inputId = 'size',
+                        label='Reset Count',
+                        10, min = 0, max = 100, step = NA,
+                        width = 100
+                ),
+                div(style="display:inline-block;vertical-align:top;",
+                    actionButton(inputId='set', label='set count')
+                )
+           ),
+           div( numericInput(inputId = 'selectedInput',
+                             label='selectedInput',
+                             1, min = 0, max = 100, step = NA,
+                             width = 100
+           ),
+           actionButton(inputId='resetSelected', label='resetSelected') ),
+           div(
+               numericInput(inputId = 'rowPosInput',
+                             label='rowPosInput',
+                             1, min = 0, max = 100, step = NA,
+                             width = 100
+               ),
+               actionButton(inputId='insertRow', label='insertRow') ,
+               actionButton(inputId='removeRow', label='removeRow')
+           )
         )
     )
 )
@@ -57,6 +82,18 @@ server <- function(input, output, session) {
     output$group=renderText(input$myTibRowCntrl$group)
     output$order=renderText(input$myTibRowCntrl$order)
     output$count=renderText(length(input$myTibRowCntrl$order))
+    observeEvent(input$resetSelected,{
+        updateRowPicker(session, "myTibRowCntrl", selectRow = input$selectedInput)
+    })
+    observeEvent(input$insertRow,{
+        pos<-input$rowPosInput
+        newVal=sample.int(100,1)
+        updateRowPicker(session, "myTibRowCntrl", insertRow = pos)
+    })
+    observeEvent(input$removeRow,{
+        pos<-input$rowPosInput
+        updateRowPicker(session, "myTibRowCntrl", deleteRow = pos)
+    })
 }
 
 # Run the application
